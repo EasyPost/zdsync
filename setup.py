cb8c5dcfbb3d79205d54faed013f76ad25e8b9e8
Andrew Tribone <tribone@easypost.com>
2019-03-23 15:27:03 -0500

    python version requirment and long description

--- setup.py
+++ setup.py
@@ -11,12 +11,15 @@ setup(
     author_email="oss@easypost.com",
     url="https://github.com/easypost/zdsync",
     description="Command-line tool for syncing Zendesk environments",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
     license="ISC",
     install_requires=["zenpy"],
-    packages=find_packages(exclude=['tests']),
+    packages=find_packages(exclude=["tests"]),
+    python_requires=">3.5",
     entry_points={
-        'console_scripts': [
-            'zdsync = zdsync.cli:main',
+        "console_scripts": [
+            "zdsync = zdsync.cli:main",
         ]
     },
     classifiers=[
